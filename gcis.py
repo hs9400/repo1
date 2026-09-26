fe1909a2897995ce90b2f54161ed04eb40a5c235
Hafsa Sultana <hs9400@rit.edu>
2026-09-26 10:51:42 +0400

    added energy_status function

--- gcis.py
+++ gcis.py
@@ -15,17 +15,50 @@ def check_range(device):
         return [0.01,0.10]
     elif device == 'Television':
         print('normal range is 0.05 - 0.50 kWh')
+        return [0.05,0.50]
     elif device == 'Refrigerator':
         print('normal range is 0.10 - 1.50 kWh')
+        return [0.10,1.50]
     elif device == 'Washing Machine':
         print('normal range is 0.30 - 2.50 kWh')
+        return [0.30,2.50]
     elif device == 'Air Conditioner':
         print('normal range is 0.50 - 5.00 kWh')
+        return [0.50,5.00]
     else:
         print('Unknown Device!')
-    normal_range=False
-    high_range=False
-    critical_range=False
 
-c=(check_range('LED Light'))
-print(type(c))
+def energy_status(range,energy_consumption):
+    """
+        Description:
+            The function takes a device and its energy 
+            consumption and returns if it is normal,high or critical.
+            where high is upto 20% higher and critical is 50% higher  
+        Parameters:
+            range (list) - it is the list of the devices normal energy range. 
+            energy_comsumption (float) - given energy consumed by the device
+        Returns:
+            string, if the energy consumption of the device is within normal, high or critical range.  
+    """
+
+    critical=range[-1]+((range[-1]*50)/100)
+    high=range[-1]+((range[-1]*20)/100)
+    if energy_consumption>=critical:
+        return 'Reading is Critical'
+    elif energy_consumption>=high:
+        return 'Reading is High'
+    elif energy_consumption>=range[0] and energy_consumption<=range[1]:
+        return 'Reading is Normal'
+    else:
+        return 'Reading is Low/Invalid'
+
+    
+print(energy_status([0.05,0.50],0))
+    
+
+def main():
+    device=str(input('enter device name: '))
+    x=check_range(device)
+
+    EC=float(input('enter energy comsumption of device: '))
+    y=energy_status(x,EC)
